11bed211ac7e7576c9b330910dc5e33fd2dc8545
rejuvyesh <rejuvyesh@gmail.com>
2013-10-30 20:25:06 +0530

    copy project files from archux by @pankajmore

---
+++ jsn.py
@@ -0,0 +1,6 @@
+import json
+import pickle
+obj = pickle.load(open('save.p', 'rb'))
+with open("save.json", "w") as out: json.dump(obj, out,sort_keys=True, indent=4)
+ob = pickle.load(open('dict.p', 'rb'))
+with open("dict.json", "w") as outfile: json.dump(ob, outfile,sort_keys=True, indent=4)
