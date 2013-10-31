104998ef80cfc981260ce06d9ee2bf20b5296a1b
rejuvyesh <rejuvyesh@gmail.com>
2013-10-31 21:45:34 +0530

    txt dumping works finally

--- poars.py
+++ poars.py
@@ -6,7 +6,8 @@ import requests
 import re
 import pickle
 import time
-from subprocess import call
+import subprocess
+import sys
 from bs4 import BeautifulSoup
 import os
 url = 'http://172.26.142.66:4040/Common/CourseListing.asp'
@@ -14,15 +15,13 @@ url = 'http://172.26.142.66:4040/Common/CourseListing.asp'
 response = requests.get(url)
 
 soup = BeautifulSoup(response.content)
-print(soup.prettify)
 
 regex = re.compile("^[\s]+([A-Z]+[A-Z]*[0-9]+[A-Z0-9]*)[\n]", re.MULTILINE)
 cont = regex.findall(soup.prettify())
 pickle.dump(cont, open('sublist.p', 'wb'))
-print(cont)
 
 response.close
-scrap = 'n'
+scrap = 'y'
 obj = pickle.load(open('sublist.p', 'rb'))
 if scrap == 'y':
     for i in obj:
@@ -34,14 +33,9 @@ if scrap == 'y':
             time.sleep(2)
             oars = requests.get(url, timeout=1)
             soup = BeautifulSoup(oars.content)
-
-            print((type(soup.prettify)))
             fout.write(oars.text)
-            oars.close
-            #fout.write(soup.get_text())
-            # Still not working, using zsh for now
-            if call("elinks -dump " + i + ".html" + ">" + i + ".txt", shell=True) == 0:
-                print("")
-                #call("rm -f " + i + ".html", shell=True)
-            else:
-                print(i)
+            oars.close()
+            outfile = i + ".txt"
+            cmd = "elinks -dump " + i + ".html"
+            with open(outfile, "w") as output_f:
+                p = subprocess.Popen(cmd, stdout=output_f, shell=True)
