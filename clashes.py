621426de8faed1c996e108587e07e455752917df
rejuvyesh <rejuvyesh@gmail.com>
2013-11-01 19:24:56 +0530

    writing to csv file now

--- clashes.py
+++ clashes.py
@@ -2,4 +2,20 @@
 # Copyright rejuvyesh <rejuvyesh@gmail.com>, 2013
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
-import simplejson
+import simplejson as json
+
+data = open('dict.json')
+js = json.load(data)
+sch = []
+for k in js.keys():
+    sch.append(k+": "+js[k]['Schedule'])
+
+# timesort = sorted(js, key=lambda x: (js[x]['Schedule']))
+sli = []
+for k in js.keys():
+    sli.append(js[k]['Schedule'])
+
+l = list(zip(js.keys(), sli))
+sl = sorted(l, key=lambda x: x[1])
+fout = open('sorted.csv', 'wt')
+fout.write("\n".join('%s,%s' % x for x in sl))
